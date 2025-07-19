b2cf6b2f3848ab5624266a3d50739013bdd54649
HossamAhmadMohamed <ugs.141252@ci.suez.edu.eg>
2025-07-19 14:54:57 +0300

    splash_screen_donee

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppleIcon
+  /// assets/images/apple_icon.svg
+  static const String imagesAppleIcon = "assets/images/apple_icon.svg";
+
+  /// Assets for imagesCamionLogo
+  /// assets/images/camion_logo.png
+  static const String imagesCamionLogo = "assets/images/camion_logo.png";
+
+  /// Assets for imagesFacbookIcon
+  /// assets/images/facbook_icon.svg
+  static const String imagesFacbookIcon = "assets/images/facbook_icon.svg";
+
+  /// Assets for imagesForgotPasswordLock
+  /// assets/images/forgot_password_lock.png
+  static const String imagesForgotPasswordLock = "assets/images/forgot_password_lock.png";
+
+  /// Assets for imagesGoogleIcon
+  /// assets/images/google_icon.svg
+  static const String imagesGoogleIcon = "assets/images/google_icon.svg";
+
+  /// Assets for imagesSplash12
+  /// assets/images/splash_12.png
+  static const String imagesSplash12 = "assets/images/splash_12.png";
+
+  /// Assets for imagesSplashScreen
+  /// assets/images/splash_screen.png
+  static const String imagesSplashScreen = "assets/images/splash_screen.png";
+}
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:camion/main_production.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:camion/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
